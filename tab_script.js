1503650ddb15a3d6a6be2ac7cf3ace494c51db3b
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-04-09 10:17:18 +0300

    2

--- tab_script.js
+++ tab_script.js
@@ -1,24 +1,24 @@
 
 $("#second-btn").on("click", function(){
     $(".window_second_row_2").fadeIn(1000);
-    $(".window_second_row_1").hide(500);
-    $(".window_second_row_3").hide(500);
+    $(".window_second_row_1").hide(100);
+    $(".window_second_row_3").hide(100);
     $("#third-btn").removeClass("btn_active");
     $("#first-btn").removeClass("btn_active");
     $(this).addClass("btn_active")
 })
 $("#first-btn").on("click", function(){
     $(".window_second_row_1").fadeIn(1000);
-    $(".window_second_row_2").hide(500);
-    $(".window_second_row_3").hide(500);
+    $(".window_second_row_2").hide(100);
+    $(".window_second_row_3").hide(100);
     $("#third-btn").removeClass("btn_active");
     $("#second-btn").removeClass("btn_active");
     $(this).addClass("btn_active")
 })
 $("#third-btn").on("click", function(){
     $(".window_second_row_3").fadeIn(1000);
-    $(".window_second_row_2").hide(500);
-    $(".window_second_row_1").hide(500);
+    $(".window_second_row_2").hide(100);
+    $(".window_second_row_1").hide(100);
     $("#second-btn").removeClass("btn_active");
     $("#first-btn").removeClass("btn_active");
     $(this).addClass("btn_active");
